ead4e0699532bd1cfee51f68b2bade64a0c40720
Max <max@zptx.dev>
2023-02-27 12:34:01 +0800

    feat: initial commit

---
+++ docs/intro.md
@@ -0,0 +1,22 @@
+---
+sidebar_position: 1
+slug: '/'
+---
+
+# Home
+
+Welcome to **ZPTX DevGuide**!
+
+## Who is this for?
+
+This guide is intended for anyone who needs help with:
+
+-   **Developing for** ZPTX projects (contributing)
+-   **Modifying** ZPTX projects (forking)
+-   **Using** ZPTX projects (integrating libraries into your own projects)
+
+For help with using our bots, please refer to [**ZPTX Guide**](https://guide.zptx.dev).
+
+## Getting started
+
+Refer to the sidebar for a list of topics.
---
+++ docusaurus.config.js
@@ -0,0 +1,90 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const organizationName = "ZPTXDev";
+const projectName = "devguide";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ZPTX DevGuide',
+  tagline: 'Developer guide for ZPTX projects',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: `https://devguide.zptx.dev`,
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: `/`,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName, // Usually your GitHub org/user name.
+  projectName, // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            `https://github.com/${organizationName}/${projectName}/edit/master/`,
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/social-card.png',
+      navbar: {
+        title: 'ZPTX DevGuide',
+        logo: {
+          alt: 'ZPTX Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          { to: "https://guide.zptx.dev", label: "ZPTX Guide", position: "left" },
+          {
+            href: 'https://github.com/ZPTXDev/devguide',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: true,
+      }
+    }),
+};
+
+module.exports = config;
